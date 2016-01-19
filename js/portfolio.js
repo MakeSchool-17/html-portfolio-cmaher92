7d89333812f03c1e265decf63d4d9b48bba5e318
cmaher92 <connor.maher@students.makeschool.com>
2016-01-18 16:35:15 -0800

    Added the form.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,12 @@
-document.onreadystatechange = function () {
-    if (document.readyState == "complete") {
-        alert('hello');
-    }
-}
+$( document ).ready(function() {
+  $("#gallery").justifiedGallery({
+    rowHeight : 200,
+    lastRow : 'justify',
+    margins : 3,
+    randomize: true
+  }).on('jg.complete', function () {
+      $('#gallery a').swipebox({
+        hideBarsDelay : false
+      });
+  });
+});
