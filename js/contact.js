595204b30da5a0d86d33607c19fe4374fd42a94b
cmaher92 <connor.maher@students.makeschool.com>
2016-01-19 13:58:12 -0800

    Added some AJAX for improving the form.

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,22 @@
-    $( document ).ready(function() {
+$( document ).ready(function() {
 
-    });
+       $('form').submit(function(e) {
+           e.preventDefault();
+
+           $.ajax({
+               url: "http://formspree.io/connormmaher@gmail.com",
+               method: "POST",
+               data: {
+                   email: $('.email').val(),
+                   message: $('.message').val()
+               },
+               dataType: "json"
+           }).error(function(x, status, error){
+               alert('Uh oh, something went wrong. Please try again.');
+           }). success(function(data, status) {
+               alert('Thank you for your message. Connor will get back to you soon.');
+               $('form').find('.email, .message').val('');
+           });
+       });
+
+   });
